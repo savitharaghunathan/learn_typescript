69242b1105f91b5cfc07389704fc040e8ae3df66
Savitha Raghunathan <saveetha13@gmail.com>
2024-07-08 14:50:53 -0400

    learning fetch and stopping the fetch

--- src/app.ts
+++ src/app.ts
@@ -1,2 +1,35 @@
-let message: string = "Hello, World!";
-console.log(message);
+import fetch from "node-fetch";
+import { AbortController } from "abort-controller";
+
+const cont = new AbortController();
+const signal = cont.signal;
+
+async function getData() {
+
+    const url = "http://www.google.com";
+    try {
+        const response = await fetch(url, {signal});
+        if (!response.ok) {
+            throw new Error('${response.status}');
+        }
+        const data = await response.text();
+        console.log(data);
+    
+    }
+    catch (error) {
+        if (error.name == 'AbortError') {
+            console.log("Abort error");
+        } else {
+            console.log('eroor fecthing data:', error);
+        }
+      
+    }
+
+
+}
+
+getData();
+
+setTimeout(() => {
+    cont.abort();
+}, 100)
